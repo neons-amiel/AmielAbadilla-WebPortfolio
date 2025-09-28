84f4e4fc86568e2a51901589b97dfd2a364980b3
neons-amiel <robertamiel.abadilla@gmail.com>
2025-09-29 01:50:47 +0800

    added apostrohpe

--- frontend/amielabadilla/src/components/bubbles.jsx
+++ frontend/amielabadilla/src/components/bubbles.jsx
@@ -4,7 +4,7 @@ import styles from "/src/bubbles.module.css";
 const BubbleText = () => {
   return (
     <h2 className="lg:text-start text-center lg:text-7xl text-5xl font-bold text-teal-300 z-50 cursor-cell">
-      {"Im Amiel Abadilla".split("").map((child, idx) => (
+      {"I'm Amiel Abadilla".split("").map((child, idx) => (
         <span className={styles.hoverText} key={idx}>
           {child}
         </span>
